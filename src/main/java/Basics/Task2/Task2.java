5d282b3504f65dfcd2c16360a028c1032cbbcc2b
Nick <gamezo90@mail.ru>
2025-04-11 18:46:41 +0300

    Task 1 complete

---
+++ src/main/java/Basics/Task2/Task2.java
@@ -0,0 +1,12 @@
+package Basics.Task2;
+
+import static Basics.Task1.Task1.simplePrint;
+
+public class Task2 {
+    public static void main(String[] args) {
+        String str1 = "Hello Java";
+        String str2 = "Hello Belarus";
+        simplePrint(str1);
+        Basics.Task1.Task1.simplePrint(str2);
+    }
+}
